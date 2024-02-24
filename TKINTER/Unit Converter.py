f4049ad5590d72e9aeaf412987fae21129f2e032
elpanademates dev <121121319+elpanademates@users.noreply.github.com>
2024-02-24 12:51:57 +0100

    Unit Converter Upgrade

--- TKINTER/Unit Converter.py
+++ TKINTER/Unit Converter.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+#from time import sleep as slp
 
 m_km = 1.609344
 p_kg = 2.20462
@@ -11,64 +12,81 @@ WIN = tk.Tk()
 WIN.title("Unit Converter")
 WIN.geometry("300x200")
 
+
+
+
 def check():
     dropdown_text = menu.get()
-    dropdown2_text = dropdown2.get()
+    dropdown2_text = menu2.get()
     entry = var.get()
+
+    global label
+
+    label = tk.Label(WIN, text=f"Answer: {entry}", font=("Arial", 15))
 
     if dropdown_text == "Miles" and dropdown2_text == "Kilometers":
         label = tk.Label(WIN, text=f"Answer: {entry * m_km}", font=("Arial", 15))
         label.pack(pady=5)
 
     elif dropdown_text == "Kilometers" and dropdown2_text == "Miles":
-        label2 = tk.Label(WIN, text=f"Answer: {entry / m_km}", font=("Arial", 15))
-        label2.pack(pady=5)
+        label.config(text=f"Answer: {entry / m_km}",)
+        label.pack(pady=5)
 
     elif dropdown_text == "Pounds" and dropdown2_text == "Kilograms":
-        label3 = tk.Label(WIN, text=f"Answer: {entry / p_kg}", font=("Arial", 15))
-        label3.pack(pady=5)
+        label.config(text=f"Answer: {entry / p_kg}")
+        label.pack(pady=5)
 
     elif dropdown_text == "Kilograms" and dropdown2_text == "Pounds":
-        label4 = tk.Label(WIN, text=f"Answer: {entry * p_kg}", font=("Arial", 15))
-        label4.pack(pady=5)
+        label.config(text=f"Answer: {entry * p_kg}")
+        label.pack(pady=5)
 
     elif dropdown_text == "Feet" and dropdown2_text == "Meters":
-        label5 = tk.Label(WIN, text=f"Answer: {entry * ft_m}", font=("Arial", 15))
-        label5.pack(pady=5)
+        label.config(text=f"Answer: {entry * ft_m}")
+        label.pack(pady=5)
 
     elif dropdown_text == "Meters" and dropdown2_text == "Feet":
-        label6 = tk.Label(WIN, text=f"Answer: {entry / ft_m}", font=("Arial", 15))
-        label6.pack(pady=5)
+        label.config(text=f"Answer: {entry / ft_m}")
+        label.pack(pady=5)
 
     elif dropdown_text == "Celsius" and dropdown2_text == "Fahrenheit":
-        label7 = tk.Label(WIN, text=f"Answer: {(entry * 9/5) + cel_fah}", font=("Arial", 15))
-        label7.pack(pady=5)
+        label.config(text=f"Answer: {(entry * 9/5) + cel_fah}")
+        label.pack(pady=5)
 
     elif dropdown_text == "Fahrenheit" and dropdown2_text == "Celsius":
-        label8 = tk.Label(WIN, text=f"Answer: {(entry -32) * fah_cel}", font=("Arial", 15))
-        label8.pack(pady=5)
+        label.config(text=f"Answer: {(entry -32) * fah_cel}")
+        label.pack(pady=5)
 
     else:
-        label9 = tk.Label(WIN, text="Value Error!", font=("Arial", 20))
-        label9.pack(pady=5)
+        label.config(text="Value Error!", font=("Arial", 30))
+        label.pack(pady=5)
 
-    
+
+def continue_button_click():
+    label.destroy()
+    continue_button.place_forget()
+
+def place_continue_button():
+    continue_button.place(x=20, y=165)
+
 options = ["Miles", "Kilometers", "Kilograms", "Pounds", "Feet", "Meters", "Celsius", "Fahrenheit"]
 menu = ttk.Combobox(WIN, values=options)
 menu.place(x=20, y=120, width=80)
 
-dropdown2 = ttk.Combobox(WIN, values=options)
-dropdown2.place(x=165, y=120, width=80)
+menu2 = ttk.Combobox(WIN, values=options)
+menu2.place(x=165, y=120, width=80)
 
 dropdown_text = menu.get
-dropdown2_text = dropdown2.get
+dropdown2_text = menu2.get
 
-button = tk.Button(WIN, text="Show Answer", command=check)
+button = tk.Button(WIN, text="Show Answer", command=lambda: (check(), place_continue_button()))
 button.place(x=100, y=170)
 
 var = tk.IntVar()
 entry = tk.Entry(WIN, textvariable=var, width=15)
 entry.place(x=70, y=70)
 
+continue_button = tk.Button(WIN, text="Continue", command=continue_button_click)
 
-WIN.mainloop()
+
+if __name__ == "__main__":
+    WIN.mainloop()
